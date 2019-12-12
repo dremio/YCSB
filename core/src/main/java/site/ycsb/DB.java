057b4e7c17d86fadf06362f3837b33a2b6fe6472
TiffanyLam <tiffany@dremio.com>
2019-12-11 17:51:34 -0800

    [WIP] DX-19991 Extend the YCSB DB Interface to support custom use cases
    Change-Id: I7f3140552193f362d3b6a6383f8057a5f1688b77

--- core/src/main/java/site/ycsb/DB.java
+++ core/src/main/java/site/ycsb/DB.java
@@ -132,4 +132,36 @@ public abstract class DB {
    * @return The result of the operation.
    */
   public abstract Status delete(String table, String key);
+
+  /**
+   *
+   * @param table The name of the table.
+   * @param startRange The start range of created_time field value to filter on, start range inclusive.
+   * @param endRange The end range of created_time field value to filter on, end range inclusive.
+   * @param recordCount The number of records to retrieve.
+   * @param fields The list of fields to read, or null for all of them.
+   * @param result A Vector of HashMaps, where each HashMap is a set field/value pairs for one record.
+   * @return The result of the operation.
+   */
+  public Status scanWithCreatedTimeFilter(String table, String startRange, String endRange, int recordCount,
+                                          Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+    return Status.NOT_IMPLEMENTED;
+  }
+
+  /**
+   *
+   * @param table The name of the table.
+   * @param startKey the start range of namespaceKey, start range inclusive.
+   * @param endKey the end range of namespaceKey, end range inclusive.
+   * @param recordCount The number of records to retrieve.
+   * @param fields The list of fields to read, or null for all of them.
+   * @param result A Vector of HashMaps, where each HashMap is a set field/value pairs for one record
+   * @return The result of the operation.
+   */
+  public Status scanWithNamespaceKeyFilter(String table, String startKey, String endKey, int recordCount,
+                                           Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+    return Status.NOT_IMPLEMENTED;
+  }
+
+
 }
