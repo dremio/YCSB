2fd1248b768a9764615445911668bf58bcc0df23
Keerat Singh <ksingh@dremio.com>
2019-12-17 13:25:55 -0800

    DX-20053: Change Query Filters based on thread id and operation count
    Updated range and secondary workload to create the query filter based
    on thread id and operation count
    
    Change-Id: I31aa41040ac706edd30945e5bab2f8d786863356

--- core/src/main/java/site/ycsb/workloads/SecondaryIndexSearchWorkload.java
+++ core/src/main/java/site/ycsb/workloads/SecondaryIndexSearchWorkload.java
@@ -17,30 +17,65 @@ package site.ycsb.workloads;
 
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.Properties;
 
 import site.ycsb.ByteIterator;
 import site.ycsb.DB;
 import site.ycsb.Status;
 import site.ycsb.Workload;
+import site.ycsb.WorkloadException;
 import site.ycsb.measurements.Measurements;
 
 /**
  * Scenario 3 of the Dremio performance test.
  */
 public class SecondaryIndexSearchWorkload extends Workload {
-  @Override
-  public boolean doInsert(DB db, Object threadstate) {
-    return true;
-  }
+  private int recordCount;
 
   @Override
-  public boolean doTransaction(DB db, Object threadstate) {
+  public boolean doInsert(DB db, Object threadState) {
+    return true;
+  }
+  @Override
+  public void init(Properties p) throws WorkloadException {
+    recordCount = Integer.parseInt(p.getProperty("recordcount"));
+  }
+
+  private static class ThreadOffset {
+    public int start;
+    public int end;
+    public int count = 0;
+
+    public ThreadOffset(int threadId) {
+      /*
+          ThreadId 0: start=800, end=900
+          ThreadId 1: start=900, end=1000
+          ...
+          ...
+          ThreadId 9: start=1700, end=1800
+      */
+      this.start = (8 + threadId) * 100;
+      this.end = this.start + 100;
+    }
+  }
+
+  public Object initThread(Properties p, int myThreadId, int threadCount) throws WorkloadException {
+    return new ThreadOffset(myThreadId);
+  }
+
+
+    @Override
+  public boolean doTransaction(DB db, Object threadState) {
+    final ThreadOffset threadOffset = (ThreadOffset) threadState;
     final Status status;
     final long startTime = System.nanoTime();
     final Vector<HashMap<String, ByteIterator>> result = new Vector<>();
-    status = db.scanWithCreatedTimeFilter("jobs", "800", "2000", 50, null, result);
+    final String startRange =  Integer.toString(threadOffset.start + threadOffset.count);
+    final String endRange =  Integer.toString(threadOffset.end + threadOffset.count);
+    status = db.scanWithCreatedTimeFilter("jobs", startRange, endRange, recordCount, null, result);
+    threadOffset.count++;
     final long endTime = System.nanoTime();
-    if (result.size() < 50) {
+    if (result.size() < recordCount) {
       return false;
     }
     Measurements.getMeasurements().measure("SECONDARY_INDEX_SEARCH", (int) (endTime - startTime) / 1000);
